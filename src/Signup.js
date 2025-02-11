e213dba79970db56b8de2988686857b660f58271
724Pradeep <724pradeep@gmail.com>
2025-02-11 20:44:43 +0530

    Sign up Feacture States Completed

--- src/Signup.js
+++ src/Signup.js
@@ -1,4 +1,35 @@
+import { useState } from "react";
+
 function Signup() {
+
+  // useState Variable
+
+  var [name,setName] = useState("");
+  var [email,setEmail] = useState("");
+  var [mobile,setMobile] = useState("");
+  var [password,setPassword] = useState("");
+
+
+  function handleChangeName(event){
+    setName(event.target.value);
+  }
+
+  function handleChangeEmail(event){
+   setEmail(event.target.value);
+  }
+
+  function handleChangeMobile(event){
+    setMobile(event.target.value);
+  }
+
+  function handleChangePword(event){
+    setPassword(event.target.value);
+  }
+
+  function handleCreateAccount(){
+    console.log(name + " - " +  email + " - " + mobile + " - " +  password)
+  }
+
   return (
     <div className="container">
       <div className="row">
@@ -13,6 +44,7 @@ function Signup() {
               placeholder="Enter Your Name"
               className="form-control"
               id="signupName"
+              onChange = {event => handleChangeName(event)}
             />
           </div>
           <div className="mb-4">
@@ -24,20 +56,39 @@ function Signup() {
               placeholder="Enter Your Email"
               className="form-control"
               id="signupemail"
+              onChange={event => handleChangeEmail(event) }
             />
           </div>
           <div className="mb-4">
             <label for="signuppassword" className="mb-2 text-primary fw-medium">
-              Name
+              Mobile
+            </label>
+            <input
+              type="number"
+              placeholder="Enter Mobile Number"
+              className="form-control"
+              id="signupnumber"
+              onChange={event => handleChangeMobile(event)}
+            />
+          </div>
+          <div className="mb-4">
+            <label for="signuppassword" className="mb-2 text-primary fw-medium">
+              Password
             </label>
             <input
               type="password"
               placeholder="Enter Password"
               className="form-control"
               id="signuppassword"
+              onChange={event => handleChangePword(event)}
             />
           </div>
-          <button className="btn btn-primary">Sign Up</button>
+          <button className="btn btn-primary mb-2" onClick={event => handleCreateAccount()}>Create Account</button>
+          <br/>
+
+
+
+          {name} <br/> {email} <br/> {mobile} <br/> {password}
 
           <div className="d-flex flex-column mt-3">
             <a href="/" className="text-decoration-none">
