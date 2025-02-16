7ea0b044364a8de2bc942eac113344f824b8313b
724Pradeep <724pradeep@gmail.com>
2025-02-16 16:04:52 +0530

    Fav_list added

--- src/ProductsList.js
+++ src/ProductsList.js
@@ -8,11 +8,33 @@ function ProductsList() {
     async function apiCallig() {
       let dummyApiResponse = await axios.get("https://dummyjson.com/products");
       //   console.log(dummyApiResponse);
-        console.log(dummyApiResponse.data.products);
-      setProductsList(dummyApiResponse.data.products);
+        // console.log(dummyApiResponse.data.products);
+
+        var updatedApiResponse =  dummyApiResponse.data.products.map(product => {
+          product.is_fav = false;
+          return product
+        });
+        // console.log(updatedApiResponse);
+
+      setProductsList(updatedApiResponse);
     }
     apiCallig();
   }, []);
+
+function updateFavList(productData){
+    console.log(productData.id)
+    var tempData = productsList.map(product => {
+      if(product.id === productData.id){
+        if(productData.is_fav === false){
+          product.is_fav = true;
+        }else{
+          product.is_fav = false;
+        }
+      }
+      return product;
+    })
+    setProductsList(tempData);
+  }
 
   return (
     <div className="container">
@@ -24,6 +46,10 @@ function ProductsList() {
               <h6>{product.title}</h6>
               <p>{product.description}</p>
             </div>
+            <button className="btn btn-primary mb-4" onClick={event => updateFavList(product)}>
+              {product.is_fav === true && <spam>Remove</spam> }
+              {product.is_fav === false && <spam>Add</spam> }
+              </button>
           </div>
         ))}
       </div>
